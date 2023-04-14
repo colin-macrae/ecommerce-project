37f235f4e1cac4540ccd5bc780f3175857f14169
Colin Macrae <csmacrae@gmail.com>
2023-04-14 23:59:59 +0000

    schema

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,17 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."Products" (
+	"productId" serial NOT NULL,
+	"type" serial NOT NULL,
+	"gender" serial NOT NULL,
+	"name" serial NOT NULL,
+	"description" serial NOT NULL,
+	"details" serial NOT NULL,
+	"price" serial NOT NULL,
+	"color" serial NOT NULL,
+	CONSTRAINT "Products_pk" PRIMARY KEY ("productId")
+) WITH (
+  OIDS=FALSE
+);
