61a31ae2793532c9735da847f406a7ceaa8f8f62
Colin Macrae <csmacrae@gmail.com>
2023-04-25 01:23:35 +0000

    created CSS for shopping cart

--- client/src/pages/Shopping-cart.js
+++ client/src/pages/Shopping-cart.js
@@ -1,5 +1,53 @@
+import './Shopping-cart.css'
+
 export default function ShoppingCart() {
   return (
-    <p>Shopping Cart</p>
+    <div className="container prod-details-container cart-items-container">
+      <h1 className='cart-header'>Cart</h1>
+
+      <CartItems/>
+
+      <div className='order-tally'>
+        <div>
+          <div className='order-lineitem'>
+            <p>Subtotal</p>
+            <p className='dollar-amt'>$130</p>
+          </div>
+          <div className='order-lineitem'>
+            <p>Delivery</p>
+            <p className='dollar-amt'>FREE</p>
+          </div>
+          <div className='checkout-btn-container'>
+            <button className='checkout-btn'>
+              PROCEED TO CHECKOUT
+            </button>
+            </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+
+function CartItems() {
+  return (
+    <div className="row">
+      <div className="col-6 col-md-6 col-lg-6">
+        <img src='https://assets.pasnormalstudios.com/391/conversions/Man_Essential-Jerswey_Earth_4-5-pdp-page.jpg?v=1668716956' alt='jersey' className="cart-image" />
+      </div>
+      <div className="col-6 col-md-6 col-lg-6 cart-item-text">
+        <div>
+          <div className='name-and-price'>
+            <p>Jersey</p>
+            <p className='dollar-amt'>$155.00</p>
+          </div>
+          <p>Green</p>
+          <div className='remove-button-div'>
+            <a href='#'>Remove Item</a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
